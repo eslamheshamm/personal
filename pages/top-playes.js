78387c76ffb86c3fd701353cf0e36c56e66e6130
eslamheshamm <esl2mhesham@gmail.com>
2021-02-05 19:42:27 +0200

    adding title

--- pages/top-playes.js
+++ pages/top-playes.js
@@ -1,7 +1,8 @@
 import useSWR from "swr";
 import fetcher from "./api/fetcher";
 import Track from "./Track";
-import Layout from "../components/Layout";
+import Layout from "../components/TracksLayout";
+import Head from "next/head";
 export default function TopTracks() {
 	const { data } = useSWR("/api/top-tracks", fetcher);
 
@@ -10,6 +11,9 @@ export default function TopTracks() {
 	}
 	return data.tracks.map((track, index) => (
 		<div className="bg-gradient-to-r from-green-400 to-green-800">
+			<Head>
+				<title>Eslam, Top Tracks - Spotify</title>
+			</Head>
 			<Layout>
 				<Track ranking={index + 1} key={track.songUrl} {...track} />
 			</Layout>
